165eb96fa3868509d8ec0f6d4ab438af64ce473a
Aditya2906-as <adityakadage17@gmail.com>
2026-06-11 14:36:47 +0530

    HashRouters

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import useAuthStore from './context/authStore'
 
@@ -72,7 +72,7 @@ export default function App() {
   useEffect(() => { fetchMe() }, [])
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Toaster
         position="top-right"
         toastOptions={{
@@ -127,6 +127,6 @@ export default function App() {
         {/* ── 404 ── */}
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
